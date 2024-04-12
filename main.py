ec62bb375975d217c2599ba015b17e8ff57f0502
Ben <benasan2004@gmail.com>
2024-04-12 15:49:55 +0200

    Correción con escritura, sin print

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def bloque_condicional_if(blockName, condition, TruePath, FalsePath):
-    print(f"Bloque [{blockName}] comienza ejecución ---> if {condition}:")
-    if eval(condition):
-        print(f"La condición es TRUE")
-        return TruePath
-    else:
-        print(f"La condición es FALSE")
-        return FalsePath
+def bloque_condicional_if(file, condition, TruePath, FalsePath):
+    file = open(f"{file}", "a")
+    file.write(f"if {condition}:\n")
+    file.write(f"   return {TruePath}\n")
+    file.write(f"else:\n")
+    file.write(f"   return {FalsePath}\n")
+    file.close()
+
+file = open("sample.py", "w")
+file.write("")
+file.close()
+
+bloque_condicional_if("sample.py", "1 == 2", "73137917", "839080139")
---
+++ sample.py
@@ -0,0 +1,4 @@
+if 1 == 2:
+   return 73137917
+else:
+   return 839080139
