ceb72e72b6b882c635fb036966731a5be5832bb2
Ben <benasan2004@gmail.com>
2024-04-12 15:27:44 +0200

    Adición de las posibles rutas y  devolución del camino resultante

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def bloque_condicional_if(blockName, condition):
+def bloque_condicional_if(blockName, condition, TruePath, FalsePath):
     print(f"Bloque [{blockName}] comienza ejecución ---> if {condition}:")
     if eval(condition):
         print(f"La condición es TRUE")
-        return True
+        return TruePath
     else:
         print(f"La condición es FALSE")
-        return False
+        return FalsePath
