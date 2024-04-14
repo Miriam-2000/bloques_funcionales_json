6c47c8221854328248329189004f7e2ef5ca971b
FranHidC <f.hidalgocalvente@gmail.com>
2024-04-14 23:15:48 +0200

    Incluyo dirección de los If's

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ with open("example_backend.json", "r") as archivo_json:
 
 conexiones = []
 i = 0
-mensajes = ["Empiezo", "trigger", "cámara", "ia","conditional","conditional","plc_write","end_cycle"]
 target = ["paco"]
+condition = 0
 while target != "":
     for node in data:
         # while target != []:
@@ -16,16 +16,23 @@ while target != "":
             target = node["connectedTo"]["outputs"][0]
             data.remove(node)
             print("PRIMER NODO ENCONTRADO: ",node["schemaId"]) 
-            i +=1                
-
+            i += 1                
+                 
         if target == node["id"]:
             conexiones.append(node)
-            if node["connectedTo"]["outputs"] != [] :
+
+            if node["schemaId"] == "conditional-if-node":
+                if condition == 1:
+                    target = node["connectedTo"]["outputs"][0]
+                else:
+                    target = node["connectedTo"]["outputs"][1]
+            
+            if node["connectedTo"]["outputs"] != []:
                 target = node["connectedTo"]["outputs"][0]
                 print("ENLAZADO CON: ", node["schemaId"])
-                i +=1 
+                i += 1 
             else:
-                target=""
+                target = ""
                 print("ÚLTIMO NODO ENCONTRADO: ",node["schemaId"])
             
             data.remove(node)
