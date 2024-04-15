92a91d98c361919bff27c76525f03008d9809bf5
Ben <benasan2004@gmail.com>
2024-04-15 07:57:43 +0200

    Primera versión funcional

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+file = open(f"sample.py", "a")
+file.write("def conditional_if_node(file, condition, TruePath, FalsePath):\n")
+file.write("   if condition:\n")
+file.write("       return TruePath\n")
+file.write("   else:\n")
+file.write("       return FalsePath\n")
+file.close()
+
+
 def bloque_condicional_if(file, condition, TruePath, FalsePath):
     file = open(f"{file}", "a")
-    file.write(f"if {condition}:\n")
-    file.write(f"   return {TruePath}\n")
-    file.write(f"else:\n")
-    file.write(f"   return {FalsePath}\n")
+    file.write(f"conditional_if_node({file}, {condition}, {TruePath}, {FalsePath})\n")
     file.close()
 
 file = open("sample.py", "w")
