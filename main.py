db1794ca8dc05669eefac7ef7176cc5cd9cf0e3d
jfernandem <jfernandezm13@educantabria.es>
2024-04-15 10:23:36 +0200

    Cambios para mostrar los inputs y los outputs

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+
+with open("example_backend.json", "r") as archivo_json:
+    data = json.load(archivo_json)
+
+
+for node in data:
+    if node["schemaId"] == "end-cycle-node":
+        schema_id = node["schemaId"]
+        inputs = node["inputs"]
+        outputs = node["outputs"]
+        print("schemaId:", schema_id)
+        print("Inputs:")
+        for inp in inputs:
+            print("\tInput Id:", inp["inputId"], "| Pertenece a:", node["schemaId"])
+        print("Outputs:")
+        if not outputs:
+            print("\tNo hay más nodos, es el final del ciclo.")
+        else:
+            for out in outputs:
+                print("\tOutput Id:", out["outputId"], "| Pertenece a schemaId:", node["id"])
+        break 
